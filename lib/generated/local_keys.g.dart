9ae7727eae9aa3d64f649ea3bfc4625a48533fd4
lukas <lukaserex@gmail.com>
2024-01-03 18:34:13 +0100

    update

---
+++ lib/generated/local_keys.g.dart
@@ -0,0 +1,36 @@
+// DO NOT EDIT. This is code generated via package:easy_localization/generate.dart
+
+abstract class  LocaleKeys {
+  static const home_page_navigation_sheet_metal = 'home_page.navigation.sheet_metal';
+  static const home_page_navigation_rods = 'home_page.navigation.rods';
+  static const home_page_navigation_pipes = 'home_page.navigation.pipes';
+  static const home_page_navigation_contact = 'home_page.navigation.contact';
+  static const home_page_navigation_rest = 'home_page.navigation.rest';
+  static const home_page_navigation_calculator = 'home_page.navigation.calculator';
+  static const home_page_navigation_sub_menu_sheet_metal_hotRolled = 'home_page.navigation.sub_menu.sheet_metal.hotRolled';
+  static const home_page_navigation_sub_menu_sheet_metal_galvanized = 'home_page.navigation.sub_menu.sheet_metal.galvanized';
+  static const home_page_navigation_sub_menu_sheet_metal_checkerPlate = 'home_page.navigation.sub_menu.sheet_metal.checkerPlate';
+  static const home_page_navigation_sub_menu_sheet_metal_coldRolled = 'home_page.navigation.sub_menu.sheet_metal.coldRolled';
+  static const home_page_navigation_sub_menu_sheet_metal = 'home_page.navigation.sub_menu.sheet_metal';
+  static const home_page_navigation_sub_menu_rods_round = 'home_page.navigation.sub_menu.rods.round';
+  static const home_page_navigation_sub_menu_rods = 'home_page.navigation.sub_menu.rods';
+  static const home_page_navigation_sub_menu_pipes_steel = 'home_page.navigation.sub_menu.pipes.steel';
+  static const home_page_navigation_sub_menu_pipes_seamless = 'home_page.navigation.sub_menu.pipes.seamless';
+  static const home_page_navigation_sub_menu_pipes_seamed = 'home_page.navigation.sub_menu.pipes.seamed';
+  static const home_page_navigation_sub_menu_pipes = 'home_page.navigation.sub_menu.pipes';
+  static const home_page_navigation_sub_menu_others_closed_profiles = 'home_page.navigation.sub_menu.others.closed_profiles';
+  static const home_page_navigation_sub_menu_others_steel_angle_bars = 'home_page.navigation.sub_menu.others.steel_angle_bars';
+  static const home_page_navigation_sub_menu_others = 'home_page.navigation.sub_menu.others';
+  static const home_page_navigation_sub_menu = 'home_page.navigation.sub_menu';
+  static const home_page_navigation = 'home_page.navigation';
+  static const home_page_image_section_header = 'home_page.image_section.header';
+  static const home_page_image_section_content = 'home_page.image_section.content';
+  static const home_page_image_section_button_text = 'home_page.image_section.button_text';
+  static const home_page_image_section = 'home_page.image_section';
+  static const home_page_satisfaction_section_header = 'home_page.satisfaction_section.header';
+  static const home_page_satisfaction_section_content_p1 = 'home_page.satisfaction_section.content_p1';
+  static const home_page_satisfaction_section_content_p2 = 'home_page.satisfaction_section.content_p2';
+  static const home_page_satisfaction_section = 'home_page.satisfaction_section';
+  static const home_page = 'home_page';
+
+}
---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\lukas\\fvm\\versions\\3.3.10" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\lukas\\IdeaProjects\\stan_handel" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\lukas\\fvm\\versions\\3.3.10"
+  "PROJECT_DIR=C:\\Users\\lukas\\IdeaProjects\\stan_handel"
+  "FLUTTER_ROOT=C:\\Users\\lukas\\fvm\\versions\\3.3.10"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\lukas\\IdeaProjects\\stan_handel\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\lukas\\IdeaProjects\\stan_handel"
+  "FLUTTER_TARGET=C:\\Users\\lukas\\IdeaProjects\\stan_handel\\lib\\main.dart"
+  "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\lukas\\IdeaProjects\\stan_handel\\.dart_tool\\package_config.json"
+)
